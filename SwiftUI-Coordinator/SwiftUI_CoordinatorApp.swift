bc00d07536381319516d8bc7661bb5e3fce3add3
mculha <melih.culha.09@gmail.com>
2023-06-03 18:20:00 +0300

    Initial Commit

---
+++ SwiftUI-Coordinator/SwiftUI_CoordinatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_CoordinatorApp.swift
+//  SwiftUI-Coordinator
+//
+//  Created by Melih Çulha on 3.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_CoordinatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
